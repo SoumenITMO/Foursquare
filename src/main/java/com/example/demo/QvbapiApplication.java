547037320e6642acc250f051c92e456649e6bb25
Soumen Banerjee <soumen.banerjee@samasatech.com>
2020-09-19 23:57:16 +0530

    Heroku deploy fixup!

--- src/main/java/com/example/demo/QvbapiApplication.java
+++ src/main/java/com/example/demo/QvbapiApplication.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QvbapiApplication {
+public class QvbapiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(QvbapiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(QvbapiApplication.class, args);
